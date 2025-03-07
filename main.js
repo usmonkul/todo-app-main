034ba73b2acaf15d0c121c8f1f275068b0d49f7e
usmonkul <usmonhakimov1999@gmail.com>
2025-03-07 14:58:09 +0500

    add filter functions

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ let activeTasks = document.getElementById('activeTasks');
 let completedTasks = document.getElementById('completedTasks');
 let clearCompleted = document.getElementById('clearCompleted');
 let theme = document.getElementById('theme');
-let sun = document.getElementById('sun');
-let moon = document.getElementById('moon');
+let itemsLeft = document.getElementById('items-left');
 const themeToggle = document.getElementById("themeToggle");
 const body = document.body;
 
@@ -23,7 +22,7 @@ themeToggle.addEventListener("click", () => {
     } else {
         themeToggle.src = "./images/icon-sun.svg";
     }
-    
+
     body.classList.toggle("dark-theme");
     body.classList.toggle("light-theme");
 
@@ -61,47 +60,162 @@ function addTask() {
     renderToDoes();
 }
 
+function createToDoes(toDo, index) {
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    let label = document.createElement('label');
+    let removeBtn = document.createElement('button');
+    let removeBtnImg = document.createElement('img');
+    let checkedImage = document.createElement('img');
+    checkedImage.src = './images/icon-check.svg';
+    checkedImage.className = 'checkedImage';
+
+    removeBtn.className = 'removeBtn';
+    removeBtnImg.src = './images/icon-cross.svg';
+    removeBtn.appendChild(removeBtnImg);
+
+    span.className = 'circle';
+    if (toDo.isCompleted) {
+        span.appendChild(checkedImage);
+        span.style.background = "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
+        li.style.textDecoration = 'line-through';
+        li.style.color = 'hsl(234, 11%, 52%)';
+    }
+    span.onclick = function () {
+        toDoes[index].isCompleted  = !toDoes[index].isCompleted;
+        renderToDoes();
+    }
+    li.appendChild(span);
+    label.textContent = toDo.task;
+    li.appendChild(label);
+
+    removeBtn.onclick = function () {
+        toDoes.splice(index, 1);
+        renderToDoes();
+    }
+    li.appendChild(removeBtn);
+    list.appendChild(li);
+}
+
 function renderToDoes() {
     list.innerHTML = '';
     toDoes.forEach((toDo, index) => {
-        let li = document.createElement('li');
-        let span = document.createElement('span');
-        let label = document.createElement('label');
-        let removeBtn = document.createElement('button');
-        let removeBtnImg = document.createElement('img');
-        let checkedImage = document.createElement('img');
-        checkedImage.src = './images/icon-check.svg';
-        checkedImage.className = 'checkedImage';
-
-        removeBtn.className = 'removeBtn';
-        removeBtnImg.src = './images/icon-cross.svg';
-        removeBtn.appendChild(removeBtnImg);
-
-        span.className = 'circle';
-        if (toDo.isCompleted) {
-            span.appendChild(checkedImage);
-            span.style.background = "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
-            li.style.textDecoration = 'line-through';
-            li.style.color = 'hsl(234, 11%, 52%)';
-        }
-        span.onclick = function () {
-            toDoes[index].isCompleted  = !toDoes[index].isCompleted;
-            renderToDoes();
-        }
-        li.appendChild(span);
-        label.textContent = toDo.task;
-        li.appendChild(label);
-
-        removeBtn.onclick = function () {
-            toDoes.splice(index, 1);
-            renderToDoes();
-        }
-        li.appendChild(removeBtn);
-        list.appendChild(li);
+        createToDoes(toDo, index);
     });
 }
 
+function clearCompletedTasks() {
+    toDoes = toDoes.filter(toDo => !toDo.isCompleted);
+    renderToDoes();
+}
+
+function showAllTasks() {
+    allTasks.style.color = 'hsl(220, 98%, 61%)';
+    activeTasks.style.color = 'hsl(234, 11%, 52%)';
+    completedTasks.style.color = 'hsl(234, 11%, 52%)';
+    renderToDoes();
+}
+
+function showActiveTasks() {
+    allTasks.style.color = 'hsl(234, 11%, 52%)';
+    activeTasks.style.color = 'hsl(220, 98%, 61%)';
+    completedTasks.style.color = 'hsl(234, 11%, 52%)';
+    list.innerHTML = '';
+    toDoes.forEach((toDo, index) => {
+        if (!toDo.isCompleted) {
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let label = document.createElement('label');
+            let removeBtn = document.createElement('button');
+            let removeBtnImg = document.createElement('img');
+            let checkedImage = document.createElement('img');
+            checkedImage.src = './images/icon-check.svg';
+            checkedImage.className = 'checkedImage';
+
+            removeBtn.className = 'removeBtn';
+            removeBtnImg.src = './images/icon-cross.svg';
+            removeBtn.appendChild(removeBtnImg);
+
+            span.className = 'circle';
+            if (toDo.isCompleted) {
+                span.appendChild(checkedImage);
+                span.style.background = "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
+                li.style.textDecoration = 'line-through';
+                li.style.color = 'hsl(234, 11%, 52%)';
+            }
+            span.onclick = function () {
+                toDoes[index].isCompleted  = !toDoes[index].isCompleted;
+                renderToDoes();
+            }
+            li.appendChild(span);
+            label.textContent = toDo.task;
+            li.appendChild(label);
+
+            removeBtn.onclick = function () {
+                toDoes.splice(index, 1);
+                renderToDoes();
+            }
+            li.appendChild(removeBtn);
+            list.appendChild(li);
+        }
+    });
+}
+function showComplatedTasks() {
+    allTasks.style.color = 'hsl(234, 11%, 52%)';
+    activeTasks.style.color = 'hsl(234, 11%, 52%)';
+    completedTasks.style.color = 'hsl(220, 98%, 61%)';
+    list.innerHTML = '';
+    toDoes.forEach((toDo, index) => {
+        if (toDo.isCompleted) {
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let label = document.createElement('label');
+            let removeBtn = document.createElement('button');
+            let removeBtnImg = document.createElement('img');
+            let checkedImage = document.createElement('img');
+            checkedImage.src = './images/icon-check.svg';
+            checkedImage.className = 'checkedImage';
+
+            removeBtn.className = 'removeBtn';
+            removeBtnImg.src = './images/icon-cross.svg';
+            removeBtn.appendChild(removeBtnImg);
+
+            span.className = 'circle';
+            if (toDo.isCompleted) {
+                span.appendChild(checkedImage);
+                span.style.background = "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
+                li.style.textDecoration = 'line-through';
+                li.style.color = 'hsl(234, 11%, 52%)';
+            }
+            span.onclick = function () {
+                toDoes[index].isCompleted  = !toDoes[index].isCompleted;
+                renderToDoes();
+            }
+            li.appendChild(span);
+            label.textContent = toDo.task;
+            li.appendChild(label);
+
+            removeBtn.onclick = function () {
+                toDoes.splice(index, 1);
+                renderToDoes();
+            }
+            li.appendChild(removeBtn);
+            list.appendChild(li);
+        }
+    });
+}
+
+
+
 renderToDoes();
+
+
+clearCompleted.onclick = clearCompletedTasks;
+allTasks.onclick = showAllTasks;
+activeTasks.onclick = showActiveTasks;
+completedTasks.onclick = showComplatedTasks;
+
+itemsLeft.textContent = toDoes.filter(toDo => !toDo.isCompleted).length + ' items left';
 
 
 inputValue.addEventListener("keypress", (event) => {
